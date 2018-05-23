d02edd340cdfd6f4120d5476a211f3fdbd371521
Ignacio Anaya <ignacio.anaya89@gmail.com>
2018-05-23 15:35:04 -0300

    Demo

---
+++ Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="click">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Click',
+      message: 'Hello!'
+    }
+  },
+
+  methods: {
+    click () {
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  color: tomato;
+  height: 40px;
+}
+</style>
+
+
